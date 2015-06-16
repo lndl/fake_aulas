f6493f767d1c682613ff01a8967139698ea28b14
lndl <laudleon@gmail.com>
2015-06-16 12:14:59 -0300

    Changes in courses

--- main.rb
+++ main.rb
@@ -1,48 +1,96 @@
 require 'sinatra'
 require_relative 'core_ext/string'
 
+set :bind, '0.0.0.0'
+set :port, '8080'
+
 COURSES = {
-  'Assembler' => {
-    7 => 'Seminario C (Assembler)',
-    8 => 'Seminario C (Assembler)',
-    9 => 'AyED (Assembler)',
-    10 => 'AyED (Assembler)',
-    11 => 'Objetos I (Assembler)',
-    12 => 'Objetos I (Assembler)',
-    20 => 'IBD (Assembler)',
-    21 => 'IBD (Assembler)',
-    22 => 'IBD (Assembler)'
-  },
-  'FORTRAN' => {
-    7 => 'Seminario C (FORTRAN)',
-    8 => 'Seminario C (FORTRAN)',
-    9 => 'AyED (FORTRAN)',
-    10 => 'AyED (FORTRAN)',
-    11 => 'Objetos I (FORTRAN)',
-    12 => 'Objetos I (FORTRAN)',
-    20 => 'IBD (FORTRAN)',
-    21 => 'IBD (FORTRAN)',
-    22 => 'IBD (FORTRAN)'
-  },
-  'Lisp' => {
-    7 => 'Seminario C (Lisp)',
-    8 => 'Seminario C (Lisp)',
-    9 => 'AyED (Lisp)',
-    10 => 'AyED (Lisp)',
-    11 => 'Objetos I (Lisp)',
-    12 => 'Objetos I (Lisp)',
-    20 => 'IBD (Lisp)',
-    21 => 'IBD (Lisp)',
-    22 => 'IBD (Lisp)'
-  }
+  'Aula 1 - PL/1' => [
+    nil,
+    {
+      8 => 'Programación concurrente',
+      9 => 'Programación concurrente',
+      10 => 'Programación concurrente'
+    },
+    {
+      8 => 'Diseño centrado en el usuario',
+      9 => 'Diseño centrado en el usuario',
+      10 => 'Diseño centrado en el usuario'
+    },
+    {
+      8 => 'Diseño centrado en el usuario',
+      9 => 'Diseño centrado en el usuario',
+      10 => 'Diseño centrado en el usuario'
+    },
+    nil,
+    nil,
+    nil
+  ],
+  'Aula 2 - APL' => [
+    nil,
+    nil,
+    {
+      8 => 'Introducción a la computación móvil',
+      9 => 'Introducción a la computación móvil',
+      10 => 'Introducción a la computación móvil'
+    },
+    {
+      8 => 'Introducción a la computación móvil',
+      9 => 'Introducción a la computación móvil',
+      10 => 'Introducción a la computación móvil'
+    },
+    nil,
+    nil,
+    nil
+  ],
+  'Aula 3 - COBOL' => [
+    nil,
+    nil,
+    {
+      8 => 'LIBRE',
+      9 => 'Curso universidad',
+      10 => 'Curso universidad'
+    },
+    {
+      8 => 'LIBRE',
+      9 => 'Curso universidad',
+      10 => 'Curso universidad'
+    },
+    nil,
+    nil,
+    nil
+  ],
+  'Aula 4 - LISP' => [
+    nil,
+    nil,
+    {
+      8 => 'Organización de computadoras',
+      9 => 'Organización de computadoras',
+      10 => 'Organización de computadoras'
+    },
+    {
+      8 => 'Organización de computadoras',
+      9 => 'Organización de computadoras',
+      10 => 'Organización de computadoras'
+    },
+    nil,
+    nil,
+    nil
+  ],
 }
 
 def select_course(aproximated_name)
   courses = COURSES
   choosen_course = courses.keys.max_by { |c| c.downcase.similarity aproximated_name.downcase }
   schedule = courses[choosen_course]
-  current_hour = Time.now.hour
-  schedule[current_hour] || 'No se han encontrado horarios'
+  current_datetime = Time.now - 10800
+  current_day  = current_datetime.wday
+  current_hour = current_datetime.hour
+  schedule[current_day][current_hour]
+rescue Exception => e
+  p schedule
+  p current_day, current_hour
+  'No se han encontrado horarios'
 end
 
 get '/' do
